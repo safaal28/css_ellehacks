fdc1b2a147048b19bc7b4c8962525a8d1d376906
Saadia Shahid <saadia.shahid@torontomu.ca>
2025-02-15 14:26:42 -0500

    testing

--- App.py
+++ App.py
@@ -1,27 +1,28 @@
 import streamlit as st
-from analysis import analyze_conversation
+#from analysis import analyze_conversation
 from report import display_report
-import os
 
+import os
 from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-#print(os.getenv("OPENAI_API_KEY"))
+openai_key = os.getenv("OPENAI_API_KEY")
+print("open ai key", openai_key)
 
-# openai_key = os.getenv("OPENAI_API_KEY")
-# google_key = os.getenv("GOOGLE_CLOUD_API_KEY")
-# anthropic_key = os.getenv("ANTHROPIC_API_KEY")
+google_key = os.getenv("GOOGLE_CLOUD_API_KEY")
+print("google cloud key", openai_key)
+anthropic_key = os.getenv("ANTHROPIC_API_KEY")
+print("anthropic key", openai_key)
 
-# if not openai_key:
-#     raise ValueError("Error: OpenAI API key is missing!")
+if not openai_key:
+    raise ValueError("Error: OpenAI API key is missing!")
 
-# if not google_key:
-#     raise ValueError("Error: Google Cloud API key is missing!")
+if not google_key:
+    raise ValueError("Error: Google Cloud API key is missing!")
 
-# if not anthropic_key:
-#     raise ValueError("Error: Anthropic API key is missing!")
-
+if not anthropic_key:
+    raise ValueError("Error: Anthropic API key is missing!")
 
 st.title("Relationship Insights Analyzer")
 
